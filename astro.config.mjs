45e22b124f8cf01da7aa64c678dd94c1a85b1512
Vinoth Kumar <vinoth.akithon@gmail.com>
2026-06-28 18:49:36 +0530

    Adding favicon and deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.URL || "https://vinoth-akithon.github.io",
+  base: process.env.BASE_URL || "/3dstructure_v4",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
